b8e9fa2bd1218aba5e5dc8d3d135a5e2f9dd0a2b
CyrilCheru <cy.cheru@gmail.com>
2019-09-26 17:21:11 +0200

    Mise en place des pages avec structures de la base de données

---
+++ src/Controller/AdController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Repository\AdRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads_index")
+     */
+    public function index(AdRepository $repo)
+    {
+        $repo = $this->getDoctrine()->getRepository(Ad::class);
+
+        $ads = $repo->findAll();
+
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads,
+        ]);
+    }
+
+    /**
+     * Afficher une seule annonce
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     * 
+     * @return Response
+     */
+    public function show(Ad $ad) {
+        // recupération de l'annonce qui correspond au slug
+        // $ad = $repo->findOneBySlug($slug);
+
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad
+        ]);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Ad;
+use Faker\Factory;
+
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,9 +13,40 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('Fr-fr');
+        
+        
+        for($i=1; $i<=30; $i++) {
 
+            $ad = new Ad();
+            $title = $faker->sentence();
+            
+            $image = $faker->imageUrl(1000, 350);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>';
+
+
+            $ad->setTitle($title)
+                ->setPrice(mt_rand(40, 80))
+                ->setIntroduction($introduction)
+                ->setContent($content)
+                ->setCoverImage($image)
+                ->setRooms(mt_rand(1, 5));
+
+            for($j=1; $j <= mt_rand(2, 5); $j++) {
+                $image = new Image();
+
+                $image->setUrl($faker->imageUrl())
+                    ->setCaption($faker->sentence())
+                    ->setAd($ad);
+
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+
+        }
+        
         $manager->flush();
     }
 }
